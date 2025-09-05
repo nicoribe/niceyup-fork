79871524ee05109a10a87ea67f41af07c175bd3a
Nicolas Baldoino <nicolasbaldoino@outlook.com>
2025-09-05 20:40:22 -0300

    feat(env): add PORT to global environment variables and update server logging - Included PORT in turbo.json for enhanced environment configuration. - Updated server.ts to log the active port, improving clarity during server startup.

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -2,5 +2,5 @@ import { env } from '@workspace/env'
 import { app } from './app'
 
 app.listen({ port: env.PORT || 3333 }).then(() => {
-  console.log('HTTP server running!')
+  console.log('HTTP server running on port', env.PORT || 3333)
 })
